1f818dab67489281246fcb6d71085911ef5eb0cf
Critiek <jumbra99@gmail.com>
2020-07-24 19:08:37 +0930

    saving progress

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'help',
+	description: 'help',
+	execute(message, args) {
+		message.channel.send('TEST');
+	},
+};
---
+++ commands/status.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'status',
+	description: 'mc server status request',
+	execute(message, args) {
+		message.channel.send('TEST');
+	},
+};
