f6f04cbd2fedee572dadcd9b5450b75ccb5c6707
Critiek <jumbra99@gmail.com>
2020-08-01 21:44:54 +0930

    heroku build

--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'botinfo',
 	description: 'general bot info',
 	execute(message, args) {
-		message.channel.send('This is a bot dedicated to near real-time status of the SuperMaxxii Minecraft server. \nIt updates about every **3-5 minutes**.');
+		message.channel.send('This is a bot dedicated to near real-time status of the SuperMaxxii Minecraft server. \nIt is about **30 seconds - 1 minute** behind the server.');
 	},
 };
